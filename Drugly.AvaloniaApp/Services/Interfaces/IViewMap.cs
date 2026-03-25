d0b5c9cabcffc184b87aa0a850026cfc4c35c062
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2026-03-25 13:06:17 -0400

    Add IViewMap docs

--- Drugly.AvaloniaApp/Services/Interfaces/IViewMap.cs
+++ Drugly.AvaloniaApp/Services/Interfaces/IViewMap.cs
@@ -5,7 +5,18 @@ namespace Drugly.AvaloniaApp.Services.Interfaces;
 
 public interface IViewMap
 {
+    /// <summary>Creates a specified view with its associated viewmodel as the data context.</summary>
+    /// <typeparam name="TView">The type of the view to create.</typeparam>
+    /// <returns>The created view.</returns>
     TView CreateView<TView>() where TView : Control;
+
+    /// <summary>Creates a view based on its associated viewmodel, with the viewmodel as the data context.</summary>
+    /// <typeparam name="TViewModel">The type of the viewmodel to create a view from.</typeparam>
+    /// <returns>The created view.</returns>
     Control CreateViewFromVm<TViewModel>() where TViewModel : ViewModelBase;
+
+    /// <summary>Creates a view based on its associated viewmodel.</summary>
+    /// <param name="vmType">The type of the viewmodel to create a view from.</param>
+    /// <returns>The created view.</returns>
     Control CreateViewFromVmNoDataContext(Type vmType);
 }
