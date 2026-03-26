9af06dcd1cbe816d06665fb73c1a4c4c74c6e373
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2026-03-26 09:05:56 -0400

    Update PrescriptionState

--- Drugly.DTO/PrescriptionState.cs
+++ Drugly.DTO/PrescriptionState.cs
@@ -7,14 +7,19 @@ public enum PrescriptionState
 {
     /// <summary>Default state - doctor hasn't finalized the order yet.</summary>
     DoctorPrescription,
-    /// <summary>Another doctor must sign off to continue.</summary>
-    SecondOpinionNeeded,
+
     /// <summary>Pharmacy is processing the order.</summary>
     PharmacyProcessing,
+
     /// <summary>The pharmacy is ready for the client to pick up their prescription.</summary>
     ReadyForPickup,
+
     /// <summary>The client must pay.</summary>
     Billing,
+
     /// <summary>The client has picked up their prescription.</summary>
-    PickedUp
+    PickedUp,
+
+    /// <summary>The prescription was deleted or canceled.</summary>
+    Canceled
 }
