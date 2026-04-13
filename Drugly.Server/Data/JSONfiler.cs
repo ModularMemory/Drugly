e0bdb3f8366f3e2ff55adc9dcfa65272bce59b09
Alias413 <caviesheppard@gmail.com>
2026-04-13 13:49:22 -0400

    json file - added prescription, medication, accountdetails

--- Drugly.Server/Data/JSONfiler.cs
+++ Drugly.Server/Data/JSONfiler.cs
@@ -3,7 +3,7 @@ using Drugly.DTO;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-public static class JsonWrite
+public static class JsonWritePrescription
 {
     private static readonly JsonSerializerOptions _options = new()
     {
@@ -18,7 +18,7 @@ public static class JsonWrite
     }
 }
 
-public static class JsonRead
+public static class JsonReadPrescription
 {
     private static readonly JsonSerializerOptions _options = new()
     {
@@ -33,5 +33,71 @@ public static class JsonRead
 
         var json = File.ReadAllText(filePath);
         return JsonSerializer.Deserialize<Prescription>(json, _options);
+    }
+}
+
+public static class JsonWriteMedication
+{
+    private static readonly JsonSerializerOptions _options = new()
+    {
+        WriteIndented = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
+
+    public static void SavePrescription(Medication medication, string filePath)
+    {
+        var json = JsonSerializer.Serialize(medication, _options);
+        File.WriteAllText(filePath, json);
+    }
+}
+
+public static class JsonReadMedication
+{
+    private static readonly JsonSerializerOptions _options = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
+
+    public static Medication? LoadMedication(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return null;
+
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<Medication>(json, _options);
+    }
+}
+
+public static class JsonWriteAccountDetails
+{
+    private static readonly JsonSerializerOptions _options = new()
+    {
+        WriteIndented = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
+
+    public static void SavePrescription(AccountDetails accountdetails, string filePath)
+    {
+        var json = JsonSerializer.Serialize(accountdetails, _options);
+        File.WriteAllText(filePath, json);
+    }
+}
+
+public static class JsonReadAccountDetails
+{
+    private static readonly JsonSerializerOptions _options = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
+
+    public static AccountDetails? LoadAccountDetails(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return null;
+
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<AccountDetails>(json, _options);
     }
 }
