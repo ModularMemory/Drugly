98fe646dd1ec4f789c1d339adc8728df6b54ec3e
Alias413 <caviesheppard@gmail.com>
2026-04-14 23:13:00 -0400

    Prescription Database added

--- Drugly.Server/Services/PrescriptionDatabaseService.cs
+++ Drugly.Server/Services/PrescriptionDatabaseService.cs
@@ -5,28 +5,57 @@ namespace Drugly.Server.Services;
 
 public class PrescriptionDatabaseService : IHostedService, IPrescriptionDatabaseService
 {
+    private readonly Dictionary<Guid, Prescription> _prescriptions = new();
+    private readonly string _folderPath = "Prescriptions";
     public Task<Prescription> GetPrescriptionById(Guid id)
     {
-        throw new NotImplementedException();
+        _prescriptions.TryGetValue(id, out var prescription);
+        return Task.FromResult(prescription);
     }
 
     public Task SetPrescriptionById(Guid id, Prescription prescription)
     {
-        throw new NotImplementedException();
+        _prescriptions[id] = prescription;
+
+        var filePath = Path.Combine(_folderPath, $"{id}.json");
+        JsonWritePrescription.SavePrescription(prescription, filePath);
+
+        return Task.CompletedTask;
     }
 
     public Task<List<Prescription>> GetAllPrescriptionsByAccountId(Guid accountId)
     {
-        throw new NotImplementedException();
+        var result = _prescriptions.Values
+          .Where(p => p.PatientId == accountId)
+          .ToList();
+
+        return Task.FromResult(result);
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (!Directory.Exists(_folderPath))
+            Directory.CreateDirectory(_folderPath);
+
+        var files = Directory.GetFiles(_folderPath, "*.json");
+
+        foreach (var file in files)
+        {
+            var prescription = JsonReadPrescription.LoadPrescription(file);
+
+            if (prescription != null)
+            {
+                _prescriptions[prescription.PrescriptionId] = prescription;
+            }
+        }
+
+        Console.WriteLine($"Loaded {_prescriptions.Count} prescriptions.");
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Prescription service stopping.");
+        return Task.CompletedTask;
     }
 }
