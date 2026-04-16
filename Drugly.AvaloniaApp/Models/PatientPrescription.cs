585e145fd67ef69ab6d07585857d802811cb0a06
Cygnus <ari.bea.kess@gmail.com>
2026-04-15 21:01:41 -0400

    I have been told to push

---
+++ Drugly.AvaloniaApp/Models/PatientPrescription.cs
@@ -0,0 +1,9 @@
+﻿using Drugly.DTO;
+
+namespace Drugly.AvaloniaApp.Models;
+
+public class PatientPrescription (Prescription prescription, Medication medication)
+{
+    public Medication Medication { get; set; } = medication;
+    public Prescription Prescription { get; set; } = prescription;
+}
--- Drugly.AvaloniaApp/ViewModels/Pages/Patient/PatientMainViewModel.cs
+++ Drugly.AvaloniaApp/ViewModels/Pages/Patient/PatientMainViewModel.cs
@@ -1,25 +1,45 @@
 ﻿using Avalonia.Collections;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.Input;
+using Drugly.AvaloniaApp.Models;
+using Drugly.AvaloniaApp.Services;
 using Drugly.AvaloniaApp.Services.Interfaces;
 using Drugly.DTO;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Drugly.AvaloniaApp.ViewModels.Pages.Patient;
 
-public partial class PatientMainViewModel : ViewModelBase
+public partial class PatientMainViewModel : ViewModelBase, IPageViewModel
 {
     private readonly IPageRouter _pageRouter;
     private readonly IServiceProvider _serviceProvider;
+    private readonly IAccountDetailsService _accountDetailsService;
+    private readonly IAccountSessionService _accountSessionService;
 
-    public AvaloniaList<Prescription> Prescriptions { get; } = [];
+    public string? PageTitle => "Hello John!";
+
+    public AvaloniaList<PatientPrescription> Prescriptions { get; } = [];
+
     public PatientMainViewModel(
-        IPageRouter pageRouter, 
-        IServiceProvider serviceProvider
-        )
+        IPageRouter pageRouter,
+        IServiceProvider serviceProvider,
+        IAccountDetailsService accountDetailsService,
+        IAccountSessionService accountSessionService
+    )
     {
         _pageRouter = pageRouter;
         _serviceProvider = serviceProvider;
-    }
+        _accountDetailsService = accountDetailsService;
+        _accountSessionService = accountSessionService;
+
+        Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            if (_accountSessionService.AccountType == AccountType.Patient)
+            {
+                _accountDetailsService.GetAccountById(_accountSessionService.AccountId);
+            }
+        });
+}
 
     [RelayCommand]
     private void ViewPrescription()
