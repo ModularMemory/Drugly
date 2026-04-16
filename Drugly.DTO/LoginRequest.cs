2bb52bd633cd79689c12174f8bbfdf5cca6217e3
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2026-04-15 22:26:03 -0400

    Docs

--- Drugly.DTO/LoginRequest.cs
+++ Drugly.DTO/LoginRequest.cs
@@ -1,7 +1,11 @@
 namespace Drugly.DTO;
 
+/// <summary>A login request object.</summary>
 public sealed class LoginRequest
 {
+    /// <summary>The email.</summary>
     public required string Email { get; set; }
+
+    /// <summary>The password.</summary>
     public required string Password { get; set; }
 }
